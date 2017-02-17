dd201908f09911188536c70ec5313cabe2c56d07
典璋 <@DANN-ASUS>
2017-02-17 16:47:06 +0800

    加入專案檔案。

---
+++ ReadTheDisplay/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadTheDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int WTF;
+            //string dann;
+            WTF = 290;
+            //dann = "abc";
+            Console.WriteLine(WTF);
+            Console.WriteLine("你輸入的是:");
+            Console.WriteLine(WTF);
+            Console.ReadLine();
+
+        }
+    }
+}
